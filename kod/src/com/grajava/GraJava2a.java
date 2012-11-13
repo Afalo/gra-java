704b96384b9fff284da2594026c76f10eccaeedb
Agnieszka <ajjablonska@hotmail.com>
2012-11-13 07:14:16 +0100

    kod

---
+++ kod/src/com/grajava/GraJava2a.java
@@ -0,0 +1,26 @@
+package com.grajava;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class GraJava2a extends Activity {
+	
+	TextView Minus;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ekran2a);
+    
+        Minus = (TextView) findViewById(R.id.Minus);
+        
+    }
+}
